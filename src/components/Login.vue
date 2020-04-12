77ad19b7f3f2b7f70297f04b982a59de04f2ac0b
saadbess <saad@koubeissi.com>
2020-04-12 17:24:58 +0100

    Added email and password auth

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,6 +2,34 @@
   <aside>
     <h3>Sign in Anonymously</h3>
     <button @click="auth.signInAnonymously()">Sign in</button>
+
+    <div v-if="newUser">
+      <h3>Sign Up for a New Account</h3>
+      <a href="#" @click="newUser = false">Returning User?</a>
+    </div>
+
+    <div v-else>
+      <h3>Sign In with Email</h3>
+      <a href="#" @click="newUser = true">New User?</a>
+    </div>
+
+    <label for="email">Email</label>
+    <br />
+    <input v-model="email" placeholder="email" type="email" class="input" />
+
+    <label for="password">Password</label>
+    <br />
+    <input v-model="password" placeholder="password" type="password" class="input" />
+
+    <br />
+
+    <button
+      class="button"
+      :class="{'is-loading': loading}"
+      @click="signInOrCreateUser()"
+    >{{ newUser ? 'Sign Up' : 'Login' }}</button>
+
+    <p v-if="errorMessage">{{ errorMessage }}</p>
   </aside>
 </template>
 
@@ -11,8 +39,115 @@ import { auth } from "../firebase";
 export default {
   data() {
     return {
-      auth
+      auth,
+      newUser: false,
+      email: "",
+      password: "",
+      loading: false,
+      errorMessage: ""
     };
+  },
+
+  methods: {
+    async signInOrCreateUser() {
+      this.loading = true;
+      this.errorMessage = "";
+      try {
+        if (this.newUser) {
+          await auth.createUserWithEmailAndPassword(this.email, this.password);
+        } else {
+          await auth.signInWithEmailAndPassword(this.email, this.password);
+        }
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+
+      this.loading = false;
+    }
   }
 };
 </script>
+
+<style lang="scss">
+@-webkit-keyframes spinAround {
+  from {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(359deg);
+    transform: rotate(359deg);
+  }
+}
+@keyframes spinAround {
+  from {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(359deg);
+    transform: rotate(359deg);
+  }
+}
+
+.button {
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  align-items: center;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  box-shadow: none;
+  display: inline-flex;
+  font-size: 1rem;
+  height: 2.25em;
+  justify-content: flex-start;
+  line-height: 1.5;
+  padding-bottom: calc(0.375em - 1px);
+  padding-left: calc(0.625em - 1px);
+  padding-right: calc(0.625em - 1px);
+  padding-top: calc(0.375em - 1px);
+  position: relative;
+  vertical-align: top;
+}
+
+.button {
+  background-color: white;
+  border-color: #dbdbdb;
+  border-width: 1px;
+  color: #363636;
+  cursor: pointer;
+  justify-content: center;
+  padding-bottom: calc(0.375em - 1px);
+  padding-left: 0.75em;
+  padding-right: 0.75em;
+  padding-top: calc(0.375em - 1px);
+  text-align: center;
+  white-space: nowrap;
+}
+
+.button.is-loading::after {
+  -webkit-animation: spinAround 500ms infinite linear;
+  animation: spinAround 500ms infinite linear;
+  border: 2px solid #dbdbdb;
+  border-radius: 290486px;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  content: "";
+  display: block;
+  height: 1em;
+  position: relative;
+  width: 1em;
+}
+
+.button.is-loading {
+  color: transparent !important;
+  pointer-events: none;
+}
+
+.button.is-loading::after {
+  position: absolute;
+  left: calc(50% - (1em / 2));
+  top: calc(50% - (1em / 2));
+  position: absolute !important;
+}
+</style>
